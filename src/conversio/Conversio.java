e071248032157990b52532ff6d4e60513301d130
andreu.reyfor.dam_al <andreu.reyfor.dam_al@203-16>
2022-11-14 15:57:55 +0100

    He creat un menu amb 2 opcions de conversio entre € i £

--- src/conversio/Conversio.java
+++ src/conversio/Conversio.java
@@ -1,25 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package conversio;
 
 import java.util.Scanner;
 
-/**
- *
- * @author nil
- */
 public class Conversio {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Introdueix en €: ");
-        int valor_euros = sc.nextInt();
-                
-        double tipus_de_canvi = 0.87d;
-        System.out.println(valor_euros + " euros (€) actualment equivalen a " + (valor_euros * tipus_de_canvi) + " lliures esterlines (£)");
+
+        Scanner teclat = new Scanner(System.in);
+
+        int opcio;
+
+        do {
+            System.out.println("OPCIONS QUE POTS TRIAR");
+            System.out.println("1. Conversor d'euro a lliura.");
+            System.out.println("2. Conversor de lliura a euro.");
+            System.out.println("3. Sortir.");
+
+            System.out.println("\n Tria una opció");
+
+            opcio = teclat.nextInt();
+
+            switch (opcio) {
+                case 1:
+                    System.out.print("Introdueix quants euros vols convertir: ");
+                    int valor_euros = teclat.nextInt();
+                    double tipus_de_canvi = 0.87d;
+                    System.out.println(valor_euros + " euros (€) actualment equivalen a " + (valor_euros * tipus_de_canvi) + " lliures esterlines (£)");
+
+                    System.out.println("PREM RETURN per tornar al menu");
+                    teclat.nextLine();
+                    teclat.nextLine();
+
+                    break;
+
+                case 2:
+                    System.out.print("Introdueix quantes lliures vols convertir ");
+                    int valor_lliura = teclat.nextInt();
+                    double canvi_a_euro = 1.14d;
+                    System.out.println(valor_lliura + " lliura (£) actualment equivalen a " + (valor_lliura * canvi_a_euro) + " lliures euros €)");
+
+                    System.out.println("PREM RETURN per continuar");
+                    teclat.nextLine();
+                    teclat.nextLine();
+                    break;
+                    
+                case 3:
+                    System.out.println("Adeu, fins un altra.");
+                    break;
+
+                default:
+                    System.out.println("Opció INCORRECTA, ha de ser del 1 al 3!!");
+            }
+
+        } while (opcio != 3);
+
     }
 }
